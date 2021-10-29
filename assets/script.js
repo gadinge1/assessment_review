1020de25e3e93e9be34a4ecaaf5a856542d87d3c
Ella Gading <ellagading@ellas-air.myfiosgateway.com>
2021-10-28 22:05:01 -0400

    review

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@
 console.log('JavaScript code has loaded!')
 
 // First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
+let yourName = "Ella Gading" // HINT: Replace this with your own name!
 
 // We'll use these variables to track the counts of each cookie type
 let gb = 0      // Gingerbread
@@ -10,14 +10,38 @@ let cc = 0      // Chocolate Chip
 let sugar = 0   // Sugar Sprinkle
 
 // Code to update name display 
-document.getElementById('credit').textContent = `Created by ${yourName}`
+document.getElementById('credit').textContent = `Created by Ella Gading`
+
 
 // Event listener for clicks on the "+" button for Gingerbread cookies
 document.getElementById('add-gb').addEventListener('click', function() {
-    // HINT: You can delete this console.log after you no longer need it!
     console.log('Gingerbread + button was clicked!')
+    
+}) 
 
-    // TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
+document.getElementById('minus-gb').addEventListener('click', function(Ginger) {
+    console.log('Gingerbread - button was clicked!')
+
+
+})
+document.getElementById('add-cc').addEventListener('click', function() { 
+    console.log('Chocolate Chip + button was clicked!') 
+    
+});
+
+document.getElementById('minus-cc').addEventListener('click', function() {
+    console.log('Chocolate Chip - buttton was clicked!')
+    
+
+});
+
+document.getElementById('add-sugar').addEventListener('click', function() {
+    console.log('Sugar Sprinkle + button was clicked!')
+    
+}) 
+document.getElementById('minus-sugar').addEventListener('click', function() {
+    console.log('Sugar Sprinkle - button was clicked!')
+
 })
 
 // TODO: Hook up event listeners for the rest of the buttons
